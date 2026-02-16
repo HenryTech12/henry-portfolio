f73660c67a2811558b523118707f6701cd1815e3
Fakorode Odunayo Henry <156727288+HenryTech12@users.noreply.github.com>
2026-02-16 09:34:04 +0100

    Fix indentation in Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -61,7 +61,7 @@ const Skills = () => {
                 'HTML5 & CSS3',
                 'JavaScript (ES6+)',
                 'Responsive Design',
-                'React JS', 'TailwindCSS'
+                'React JS', 'TailwindCSS',
                 'Version Control (Git)',
             ],
             color: 'from-rose-500 to-pink-400'
